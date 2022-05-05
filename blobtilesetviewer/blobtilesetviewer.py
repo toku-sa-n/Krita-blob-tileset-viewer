4942c87718f007756a9e384b4634390b3a9c71fa
Hiroki Tokunaga <tokusan441@gmail.com>
2022-05-05 13:25:37 +0900

    Show the generated blob tileset image

--- blobtilesetviewer/blobtilesetviewer.py
+++ blobtilesetviewer/blobtilesetviewer.py
@@ -1,6 +1,7 @@
 import tempfile
+import subprocess
 from krita import (DockWidget, Krita,
-                   DockWidgetFactory, DockWidgetFactoryBase)
+                   DockWidgetFactory, DockWidgetFactoryBase, QImage, QPixmap, QSize, InfoObject)
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import (QVBoxLayout, QLabel, QPushButton, QWidget)
 
@@ -8,17 +9,21 @@ from PyQt5.QtWidgets import (QVBoxLayout, QLabel, QPushButton, QWidget)
 class BlobTilesetViewer(DockWidget):
     def __init__(self):
         super().__init__()
+        self.i = 0
 
         self.setWindowTitle("Blob tileswet viewer")
 
         self.image_label = QLabel('')
         self.image_label.setAlignment(Qt.AlignCenter)
 
+        self.message_label = QLabel('')
+
         self.open_button = QPushButton("Open")
         self.open_button.clicked.connect(self.open_button_clicked)
 
         self.layout = QVBoxLayout()
         self.layout.addWidget(self.image_label)
+        self.layout.addWidget(self.message_label)
         self.layout.addWidget(self.open_button)
 
         self.widget = QWidget()
@@ -26,13 +31,33 @@ class BlobTilesetViewer(DockWidget):
 
         self.setWidget(self.widget)
 
-        self.fp = tempfile.NamedTemporaryFile()
+        self.fp = tempfile.NamedTemporaryFile(suffix='.bmp')
 
     def __del__(self):
         self.fp.close()
 
     def canvasChanged(self, canvas):
-        pass
+        if canvas is None or canvas.view() is None:
+            return
+
+        # Krita asks how to save a PNG file even if we specify the settings
+        # with `InfoObject`.
+        # This is why we save the image as BMP.
+        canvas.view().document().exportImage(self.fp.name, InfoObject())
+        result = subprocess.run(['blob-tileset-generator', '-o',
+                                 self.fp.name, self.fp.name])
+        if result.returncode != 0:
+            self.image_label.clear()
+            self.message_label.setText(
+                'Failed to generate a blob tileset image.')
+        else:
+            self.message_label.clear()
+            thumbnail = QImage(self.fp.name)
+            thumbnail = thumbnail.scaled(
+                QSize(200, 150), Qt.KeepAspectRatio, Qt.SmoothTransformation)
+            self.image_label.setPixmap(QPixmap.fromImage(thumbnail))
+            self.message_label.setText(str(self.i))
+            self.i += 1
 
     def open_button_clicked(self):
         pass
